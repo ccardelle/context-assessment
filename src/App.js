8af9918b931e938b13d3228ccd0068f8d56fef75
Chris Cardelle <chris.cardelle@gmail.com>
2019-05-20 18:37:08 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const MyContext = React.createContext();
+
+class MyProvider extends React.Component {
+  state = {
+    x: 0
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          handleXClick: () =>
+            this.setState({
+              x: this.state.x + 1
+            })
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
 
 class A extends React.Component {
   constructor(props) {
@@ -13,71 +36,95 @@ class A extends React.Component {
     this.setState({
       x: this.state.x + 1
     });
-  }
+  };
   render() {
     return (
-      <div className="component-A">
-        <h1>Component A</h1>
-        The state in the component A is {this.state.x} <br />
-        this.handleXClick will set the state to a new value => {"this.setState({x: this.state.x + 1})"} <br />
-        <B x={this.state.x} onXClick={this.handleXClick} />
-        <D x={this.state.x} onXClick={this.handleXClick} />
-      </div>
+      <MyContext.Consumer>
+        {context => {
+          return (
+            <div className="component-A">
+              <h1>Component A</h1>
+              The state in the component A is {context.state.x} <br />
+              this.handleXClick will set the state to a new value =>{" "}
+              {"this.setState({x: this.state.x + 1})"} <br />
+              <B />
+              <D />
+            </div>
+          );
+        }}
+      </MyContext.Consumer>
     );
   }
 }
 class B extends React.Component {
   render() {
     return (
-      <div className="component-B">
-        <h1>Component B</h1>
-        this.props.x = {this.props.x} <br />
-        this.props.onXClick => (method from component A) <br />
-        <C x={this.props.x} onXClick={this.props.onXClick} />
-      </div>
+      <MyContext.Consumer>
+        {context => {
+          return (
+            <div className="component-B">
+              <h1>Component B</h1>
+              this.props.x = {context.state.x} <br />
+              this.props.onXClick => (method from component A) <br />
+              <C />
+            </div>
+          );
+        }}
+      </MyContext.Consumer>
     );
   }
 }
 class C extends React.Component {
   render() {
     return (
-      <div className="component-C">
-        <h1>Component C</h1>
-        this.props.x = {this.props.x} <br />
-        this.props.onXClick => (method passed from component B) <br />
-        <button onClick={this.props.onXClick}>{this.props.x}</button> onClick =>
-        this.props.onXClick
-      </div>
+      <MyContext.Consumer>
+        {context => {
+          return (
+            <div className="component-C">
+              <h1>Component C</h1>
+              this.props.x = {context.state.x} <br />
+              this.props.onXClick => (method passed from component B) <br />
+              <button onClick={context.handleXClick}>
+                {context.state.x}
+              </button>{" "}
+              onClick => this.props.onXClick
+            </div>
+          );
+        }}
+      </MyContext.Consumer>
     );
   }
 }
 class D extends React.Component {
   render() {
     return (
-      <div className="component-D">
-        <h1>Component D</h1>
-        this.props.x = {this.props.x} <br />
-        this.props.onXClick => (method from component A) <br />
-        <button onClick={this.props.onXClick}>{this.props.x}</button> onClick =>
-        this.props.onXClick
-      </div>
+      <MyContext.Consumer>
+        {context => {
+          return (
+            <div className="component-D">
+              <h1>Component D</h1>
+              this.props.x = {context.state.x} <br />
+              this.props.onXClick => (method passed from component B) <br />
+              <button onClick={context.handleXClick}>
+                {context.state.x}
+              </button>{" "}
+              onClick => this.props.onXClick
+            </div>
+          );
+        }}
+      </MyContext.Consumer>
     );
   }
 }
 
-
 function App() {
   return (
-    <div className="App">
-      <A />
-    </div>
+    <MyProvider>
+      <div className="App">
+        <A />
+      </div>
+    </MyProvider>
   );
 }
 
 export default App;
-
-
-
-
-
-
